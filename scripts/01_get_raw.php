db852dba46f150d763ec444267f2bdc86218c1d7
kiang <kiange@gmail.com>
2024-10-22 20:27:38 +0800

    add uuid to each item

--- scripts/01_get_raw.php
+++ scripts/01_get_raw.php
@@ -2,7 +2,7 @@
 $basePath = dirname(__DIR__);
 $reports = [
     '寺廟' => 'https://religion.moi.gov.tw/Report/temple.xml',
-    '法人教會' => 'https://religion.moi.gov.tw/Report/church.xml',
+    '教會' => 'https://religion.moi.gov.tw/Report/church.xml',
     '宗祠基金會' => 'https://religion.moi.gov.tw/Report/Ancestral-F.xml',
     '宗祠' => 'https://religion.moi.gov.tw/Report/Ancestral.xml',
     '基金會' => 'https://religion.moi.gov.tw/Report/Foundation.xml',
